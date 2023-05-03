08fa9a78da212feded586a2a07ce4656bb8008bf
JuanIsturiz <juanandresisturiz@gmail.com>
2023-05-02 21:25:16 -0400

    not working... I'll try again later..

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -20,36 +20,27 @@ declare module "next-auth" {
 }
 
 export const AuthOptions: NextAuthOptions = {
+  jwt: {
+    secret: "test",
+  },
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
-    session: ({ session, user }) => ({
+    jwt: ({ user, token }) => {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => ({
       ...session,
       user: {
         ...session.user,
-        id: user.id,
+        id: token.id,
       },
     }),
   },
-  // jwt: {
-  //   secret: "test",
-  // },
-  // session: {
-  //   strategy: "jwt",
-  // },
-  // callbacks: {
-  //   jwt: ({ user, token }) => {
-  //     if (user) {
-  //       token.id = user.id;
-  //     }
-  //     return token;
-  //   },
-  //   session: ({ session, token }) => ({
-  //     ...session,
-  //     user: {
-  //       ...session.user,
-  //       id: token.id,
-  //     },
-  //   }),
-  // },
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
@@ -64,37 +55,37 @@ export const AuthOptions: NextAuthOptions = {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    // CredentialsProvider({
-    //   type: "credentials",
-    //   name: "Credentials",
-    //   credentials: {},
-    //   async authorize(credentials, _req) {
-    //     const { email, password } = credentials as {
-    //       email: string;
-    //       password: string;
-    //     };
+    CredentialsProvider({
+      type: "credentials",
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials, _req) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
 
-    //     const dbUser = await prisma.user.findFirst({
-    //       where: { email: email.toLowerCase() },
-    //     });
-    //     if (!dbUser) throw new Error("No user found");
+        const dbUser = await prisma.user.findFirst({
+          where: { email: email.toLowerCase() },
+        });
+        if (!dbUser) throw new Error("No user found");
 
-    //     if (!dbUser.password)
-    //       throw new Error("Please Sign Up first to log in using credentials");
+        if (!dbUser.password)
+          throw new Error("Please Sign Up first to log in using credentials");
 
-    //     const passwordCheck = await bcrypt.compare(password, dbUser.password);
+        const passwordCheck = await bcrypt.compare(password, dbUser.password);
 
-    //     if (!passwordCheck) throw new Error("Password doesn't match");
-    //     const user = {
-    //       id: dbUser.id,
-    //       name: dbUser.name,
-    //       email: dbUser.email ?? "",
-    //       image: dbUser.image ?? "",
-    //     };
+        if (!passwordCheck) throw new Error("Password doesn't match");
+        const user = {
+          id: dbUser.id,
+          name: dbUser.name,
+          email: dbUser.email ?? "",
+          image: dbUser.image ?? "",
+        };
 
-    //     return user;
-    //   },
-    // }),
+        return user;
+      },
+    }),
   ],
   pages: {
     signIn: "/auth/signin",
